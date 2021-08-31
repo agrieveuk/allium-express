fef5e81f08408817a19c794b451c81d4e6c8b350
amy <algrieve1@gmail.com>
2021-08-31 16:57:25 +0100

    change name to The Allium

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,10 +6,11 @@ const Header = () => {
       <Link to='/'>
         <div className='logo-title'>
           <img
+            id='logo'
             src='https://mk0mydrtest3eri40dsq.kinstacdn.com/wp-content/uploads/2019/04/garlic_cloves_750.jpg'
             alt='Allium Logo'
           />
-          <h2>Allium Express</h2>
+          <h2>The Allium</h2>
         </div>
       </Link>
     </header>
