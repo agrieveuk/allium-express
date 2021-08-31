55bac4f2bb589c6ce226849d413d8627b5c9f49b
amy <algrieve1@gmail.com>
2021-08-31 17:32:29 +0100

    add article images and links to individual articles

--- src/components/Articles.jsx
+++ src/components/Articles.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import { getArticles } from '../utils/app';
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 
 const Articles = () => {
   const { topic } = useParams();
@@ -19,13 +19,19 @@ const Articles = () => {
           ({ article_id, title, votes, comment_count, created_at }) => {
             return (
               <li key={article_id} className='article-list-item'>
-                <h4>{title}</h4>
-                <div className='article-info'>
-                  <p>votes: {votes}</p>
-                  <p>{`${created_at.substr(12, 7)} 
+                <Link to={`/articles/${article_id}`}>
+                  <h4>{title}</h4>
+                  <img
+                    src='https://picsum.photos/400/300'
+                    alt='placeholder article image'
+                  />
+                  <div className='article-info'>
+                    <p>votes: {votes}</p>
+                    <p>{`${created_at.substr(12, 7)} 
                   ${created_at.substr(0, 10)}`}</p>
-                  <p>comments: {comment_count}</p>
-                </div>
+                    <p>comments: {comment_count}</p>
+                  </div>
+                </Link>
               </li>
             );
           }
