eb90fb61cd217cd3373f6b380032f7a2aefb4af3
amy <algrieve1@gmail.com>
2021-10-02 19:59:35 +0100

    remove trailing commas

--- src/utils/app.js
+++ src/utils/app.js
@@ -1,12 +1,12 @@
 import axios from 'axios';
 
 const newsApi = axios.create({
-  baseURL: 'https://nc-express-news.herokuapp.com/api',
+  baseURL: 'https://nc-express-news.herokuapp.com/api'
 });
 
 const getArticles = async ({ topic, sort_by, page }) => {
   const { data } = await newsApi.get('/articles', {
-    params: { topic, limit: 6, sort_by, page },
+    params: { topic, limit: 6, sort_by, page }
   });
 
   return data;
@@ -21,15 +21,15 @@ const getComments = async (article_id, page) => {
   const { data } = await newsApi.get(`/articles/${article_id}/comments`, {
     params: {
       limit: 4,
-      page,
-    },
+      page
+    }
   });
   return data.comments;
 };
 
 const patchArticle = async ({ article_id, inc_votes }) => {
   const { data } = await newsApi.patch(`/articles/${article_id}`, {
-    inc_votes,
+    inc_votes
   });
   return data.article.votes;
 };
@@ -37,14 +37,14 @@ const patchArticle = async ({ article_id, inc_votes }) => {
 const postComment = async (article_id, username, body) => {
   const { data } = await newsApi.post(`/articles/${article_id}/comments`, {
     username,
-    body,
+    body
   });
   return data.comment;
 };
 
 const patchComment = async ({ comment_id, inc_votes }) => {
   const { data } = await newsApi.patch(`/comments/${comment_id}`, {
-    inc_votes,
+    inc_votes
   });
   return data.comment.votes;
 };
@@ -61,5 +61,5 @@ export {
   patchArticle,
   postComment,
   patchComment,
-  deleteComment,
+  deleteComment
 };
