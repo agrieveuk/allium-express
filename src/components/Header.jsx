db052bbbad6bdcb483b080147aefa0131d15a133
amy <algrieve1@gmail.com>
2021-10-12 17:31:18 +0100

    update Allium Logo png

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,12 @@
 import { Link } from 'react-router-dom';
+import garlic_cloves from '../assets/garlic_cloves.png';
 
 const Header = () => {
   return (
     <header>
       <Link to='/'>
         <div className='logo-title'>
-          <img
-            id='logo'
-            src='https://mk0mydrtest3eri40dsq.kinstacdn.com/wp-content/uploads/2019/04/garlic_cloves_750.jpg'
-            alt='Allium Logo'
-          />
+          <img id='logo' src={garlic_cloves} alt='Allium Logo' />
           <h2>The Allium</h2>
         </div>
       </Link>
