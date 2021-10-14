0640531e4b46475201eeba4d0e0f980ef3aa4f0d
amy <algrieve1@gmail.com>
2021-10-14 18:53:09 +0100

    amend text area maxLength property

--- src/components/NewComment.jsx
+++ src/components/NewComment.jsx
@@ -29,7 +29,7 @@ const NewComment = ({ article_id, setComments, user: { username } }) => {
         <textarea
           id='new-comment'
           required
-          maxlength='500'
+          maxLength='500'
           placeholder='Write your comment here...'
           rows='3'
           value={newComment}
