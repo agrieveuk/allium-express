804beb0b484198d22e23658a26b5382d85275d26
amy <algrieve1@gmail.com>
2021-10-14 18:52:11 +0100

    add user avatar and name to UserProfile component

--- src/components/UserProfile.jsx
+++ src/components/UserProfile.jsx
@@ -1,13 +1,21 @@
 import { useParams } from 'react-router-dom';
+import { useUser } from '../hooks/useApi';
 import Articles from './Articles';
 
 const UserProfile = () => {
   const { username } = useParams();
+  const { user, isLoading } = useUser(username);
 
+  if (isLoading) return <h3>Any moment now...</h3>;
   return (
     <main className='user-profile'>
+      <img
+        src={user.avatar_url}
+        alt='user avatar'
+        className='user-profile-avatar-img'
+      />
       <h3>{`${username}'s user hub`}</h3>
-      <h4>Articles by this user</h4>
+      <h4>Articles by {user.name}</h4>
       <Articles />
     </main>
   );
