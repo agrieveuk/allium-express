d3c1d7a379e43597d6e3a441d53bf3476da069bc
amy <algrieve1@gmail.com>
2021-10-18 19:05:13 +0100

    change coding topic banner

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,4 +1,5 @@
 import { useParams } from 'react-router-dom';
+import codeBanner from '../assets/code-screenshot.png';
 
 const Banner = () => {
   const { topic } = useParams();
@@ -7,7 +8,7 @@ const Banner = () => {
     cooking:
       'https://images.squarespace-cdn.com/content/v1/590be7fd15d5dbc6bf3e22d0/1613775674496-PD68QUX33RKBYXUO4HZI/20201204_Babish_Cookware__0390.jpg?format=1000w',
     football: 'https://s2.dmcdn.net/v/9LRnL1RldOiLtDu_r/x1080',
-    coding: 'http://techworm.page/wp-content/uploads/2019/05/download-17.png',
+    coding: codeBanner,
   };
 
   return (
